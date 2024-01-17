bc98fe2e325ef3c3c9fa44ce3a655809af6c7192
blick <stayinblick@gmail.com>
2024-01-17 18:28:47 +0000

    bot add with options

--- src/index.js
+++ src/index.js
@@ -17,8 +17,19 @@ client.on('ready', (c) => {
 client.on('interactionCreate', (interaction) => {
     if (!interaction.isChatInputCommand()) return;
 
-    if (interaction.commandName === 'hey') {
-        interaction.reply('hey')
+    if (interaction.commandName === 'gm') {
+        interaction.reply('morning');
+    }
+
+    if (interaction.commandName === 'gn') {
+        interaction.reply('night');
+    }
+
+    if (interaction.commandName === 'add') {
+        const num1 = interaction.options.get('first-number').value;
+        const num2 = interaction.options.get('second-number').value;
+
+        interaction.reply(`The sum is ${num1 + num2}`)
     }
 })
 
