33ad4e711e5f0afca84dc7e54f188dd81ff4ca17
blick <stayinblick@gmail.com>
2024-01-17 16:24:17 +0000

    profit bot online message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-const { Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on('ready', (c) => {
+    console.log(`${c.user.username} is online`);
 })
+
+client.login(
+  "MTE5NzE3ODg2MjYyMzI2MDg2Mg.GT5H19.-Vhv5enWX-wug9wLfPjfG3XmDq9DgW7q5Xq_nI"
+);
