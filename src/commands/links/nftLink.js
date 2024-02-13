6fb1f8355e839e28711cec4d00eccf7ff814e6c0
blick <stayinblick@gmail.com>
2024-02-13 14:36:18 +0000

    nft-link command added

--- src/commands/links/nftLink.js
+++ src/commands/links/nftLink.js
@@ -1,11 +1,20 @@
-// module.exports = {
-//   name: 'magiceden_link',
-//   description: 'post a link to magic edens market place.',
-//   // devonly: Boolean,
-//   // testonly: Boolean,
-//   // options:Object[],
+const { Client, Interaction, EmbedBuilder } = require('discord.js');
 
-//   callback: (client, interaction) => {
-//     interaction.reply(`Morning ${client.ws.}ms`);
-//   },
-// };
+module.exports = {
+  /**
+   *
+   * @param {Client} client
+   * @param {Interaction} interaction
+   */
+  callback: async (client, interaction) => {
+    const embed = new EmbedBuilder()
+      .setTitle("W.O.G NFT's on MagicEden Marketplace")
+      .setDescription("Click [here](https://magiceden.io/marketplace/yoooodegens?attributes=%7B%22BACKGROUND%22%3A%5B%22Black%22%5D%7D) to check out the NFT's.")
+      .setColor("#FFC300") 
+
+    interaction.reply({ embeds: [embed] });
+  },
+
+  name: 'nft-link',
+  description: 'Check out the W.O.G NFT\'s on MagicEden Marketplace!',
+}
